1f1ebeb0be363bfa7e5174f8304cb8e33d0ce733
iplatos <platonoff9168@gmail.com>
2023-08-23 15:35:01 +0300

    added styles for primary button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 
 import { Checkbox } from './components/ui/checkbox'
+import {Button} from "@/components/ui/button";
 
 export function App() {
   const [checked, setChecked] = useState(false)
@@ -9,6 +10,11 @@ export function App() {
     <div>
       <Checkbox checked={checked} onChange={setChecked} text={'check-box'} disabled={false} />
       Hello
+        <Button variant={"tertiary"} as={"a"} href={"https://www.youtube.com/"} target={"_blank"} fullWidth> button </Button>
+        <Button  as="a" href={"/link"}>Hello</Button>
+        <Button variant="primary" href={"/link"} disabled >Hello</Button>
+        <Button>Hello</Button>
+
     </div>
   )
 }
