a9a1ac0c07c4fbbd67b69cac4ca2e60655688df1
iplatos <platonoff9168@gmail.com>
2023-08-25 16:24:58 +0300

    slider added

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
-import { useState } from 'react'
-import { Checkbox } from './components/ui/checkbox'
-import {Button} from "@/components/ui/button";
- import asd from "../src/assets/images/Layer 2.svg"
-import s from "./components/ui/button/button.module.scss"
+import {SliderDemo} from "@/components/ui/slider/Slider";
+
 
 export function App() {
-  const [checked, setChecked] = useState(false)
-
   return (
-    <div>
-        <>asd</>
-      <Checkbox checked={checked} onChange={setChecked} text={'check-box'} disabled={false} />
-      Hello
-        <div>
-            <Button className={s.button} > <img src={asd}/>Hello</Button>
-            <Button className={s.button}  variant="secondary"><img src={asd}/> qweWeW</Button>
-            <Button className={s.button}  variant="tertiary"><img src={asd}/> qweWeW</Button>
-            <Button className={s.button}  variant="tertiary">qweWeW</Button>
-
-            <Button className={s.button} as={"a"} href={"/link"} disabled variant="link">as link</Button>
+<>
+  <SliderDemo />
+</>
+         )
 
 
-
-        </div>
-    </div>
-  )
 }
--- src/components/ui/slider/Slider.tsx
+++ src/components/ui/slider/Slider.tsx
@@ -1,16 +1,27 @@
 import * as Slider from '@radix-ui/react-slider'
-import '@/components/ui/slider/SliderStyle.css'
+import s from "./Slider.module.scss"
+import {useState} from "react";
 
-const SliderDemo = () => (
-  <form>
-    <Slider.Root className="SliderRoot" defaultValue={[50]} max={100} step={1}>
-      <Slider.Track className="SliderTrack">
-        <Slider.Range className="SliderRange" />
-      </Slider.Track>
-      <Slider.Thumb className="SliderThumb" aria-label="Volume" />
-      <Slider.Thumb className="SliderThumb" aria-label="Volume" />
-    </Slider.Root>
-  </form>
-)
+export const SliderDemo = () => {
+    const [minSliderValue, setMinSliderValue] = useState(25)
+    const [maxSliderValue, setMaxSliderValue] = useState(75)
 
-export default SliderDemo
+    const changeSliderValue = (values:number[]) => {
+        values[0] === minSliderValue ? setMaxSliderValue(values[1])
+            : setMinSliderValue(values[0])
+    }
+
+    return (
+        <div className={s.SliderContainer}>
+            <span className={s.SliderValuesNumber} >{minSliderValue}</span>
+            <Slider.Root onValueChange={changeSliderValue} className={s.SliderRoot} defaultValue={[minSliderValue, maxSliderValue]} minStepsBetweenThumbs ={1} max={100} step={1}>
+                <Slider.Track className={s.SliderTrack}>
+                    <Slider.Range className={s.SliderRange}/>
+                </Slider.Track>
+                <Slider.Thumb className={s.SliderThumb} aria-label="Volume"/>
+                <Slider.Thumb className={s.SliderThumb} aria-label="Volume"/>
+            </Slider.Root>
+            <span className={s.SliderValuesNumber}>{maxSliderValue}</span>
+        </div>
+    )
+}
