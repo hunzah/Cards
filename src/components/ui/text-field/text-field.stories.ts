c207f9d1c807fe9d9503522122b5549a4a85dba2
IvanBichan <ivanbichanfe@gmail.com>
2023-08-25 20:42:19 +0200

    feat: create and add text-field component with logic and style

---
+++ src/components/ui/text-field/text-field.stories.ts
@@ -0,0 +1,98 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { TextField } from './text-field.tsx'
+
+const meta = {
+  title: 'Components/text-field',
+  component: TextField,
+  /*argTypes: {
+                                inputType: {
+                                  options: ['text', 'password'],
+                                },
+                                inputIsSearch: {
+                                  options: [true, false],
+                                },
+                              },*/
+} satisfies Meta<typeof TextField>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const TextFieldStandard: Story = {
+  args: {
+    inputType: 'text',
+    placeholder: 'Enter text',
+    inputIsSearch: false,
+  },
+}
+
+export const TextFieldStandardError: Story = {
+  args: {
+    inputType: 'text',
+    inputIsSearch: false,
+    errorMessage: 'Some error',
+  },
+}
+
+export const TextFieldLogin: Story = {
+  args: {
+    inputType: 'text',
+    inputIsSearch: false,
+    placeholder: 'Enter login',
+    inputName: 'Login',
+  },
+}
+
+export const TextFieldLoginError: Story = {
+  args: {
+    inputType: 'text',
+    inputIsSearch: false,
+    inputName: 'Login',
+    errorMessage: 'Some error',
+  },
+}
+
+export const TextFieldSearch: Story = {
+  args: {
+    inputType: 'text',
+    inputIsSearch: true,
+    placeholder: 'Enter text',
+  },
+}
+
+export const KeyDownEnterTextFieldSearch: Story = {
+  args: {
+    inputType: 'text',
+    inputIsSearch: true,
+    placeholder: 'Enter text',
+    onEnter: e => {
+      alert(e.key)
+    },
+  },
+}
+
+export const TextFieldSearchError: Story = {
+  args: {
+    inputType: 'text',
+    inputIsSearch: true,
+    errorMessage: 'Some error',
+  },
+}
+
+export const TextFieldPassword: Story = {
+  args: {
+    inputType: 'password',
+    inputIsSearch: false,
+    inputName: 'Password',
+  },
+}
+
+export const TextFieldPasswordError: Story = {
+  args: {
+    inputType: 'password',
+    inputIsSearch: false,
+    inputName: 'Password',
+    placeholder: 'Enter password',
+    errorMessage: 'Some error',
+  },
+}
