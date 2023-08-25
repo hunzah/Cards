41fdc270507b7119530db80467f5e004e39b39cb
IvanBichan <ivanbichanfe@gmail.com>
2023-08-25 21:43:53 +0200

    refactor: apply prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import {SliderDemo} from "@/components/ui/slider/Slider";
-
+import { SliderDemo } from '@/components/ui/slider/Slider'
 
 export function App() {
-  return (
-  <SliderDemo />
-  )
+  return <SliderDemo />
 }
