d651dba61e19758cd314ebb508fc02118e3e2e72
IvanBichan <ivanbichanfe@gmail.com>
2023-08-28 21:23:40 +0200

    fead: add control

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,24 +1,73 @@
-import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useController, useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { Button } from '../../ui/button'
 import { TextField } from '../../ui/text-field'
 
-type FormValues = {
-  email: string
-  password: string
-}
+import { ControlledCheckbox } from '@/components/ui/controlled/controlled-checkbox/controlled-checkbox.tsx'
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(3),
+  rememberMe: z.boolean(),
+})
+
+type FormValues = z.input<typeof loginSchema>
 
 export const LoginForm = () => {
-  const { register, handleSubmit } = useForm<FormValues>()
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver: zodResolver(loginSchema) })
 
   const onSubmit = (data: FormValues) => {
     console.log(data)
   }
 
+  /*const {
+          field: { value, onChange },
+        } = useController({
+          name: 'rememberMe',
+          control,
+          defaultValue: false,
+        })*/
+
+  const emailControl = useController({
+    name: 'email',
+    control,
+    defaultValue: '',
+  })
+
+  const passwordControl = useController({
+    name: 'password',
+    control,
+    defaultValue: '',
+  })
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <TextField {...register('email')} label={'email'} />
-      <TextField {...register('password')} label={'password'} />
+      <TextField
+        {...register('email')}
+        errorMessage={errors.email?.message}
+        value={emailControl.field.value}
+        onChangeValue={emailControl.field.onChange}
+        inputIsSearch={false}
+        inputType={'text'}
+        label={'email'}
+      />
+      <TextField
+        value={passwordControl.field.value}
+        onChangeValue={passwordControl.field.onChange}
+        errorMessage={errors.password?.message}
+        label={'password'}
+        inputIsSearch={false}
+        inputType={'password'}
+      />
+      {/* <Checkbox label={'remember me'} onChange={onChange} checked={value} />*/}
+      <ControlledCheckbox label={'remember me'} name={'rememberMe'} control={control} />
       <Button type="submit">Submit</Button>
     </form>
   )
