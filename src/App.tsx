da299adb03c472d5a77a813c037bde1a109916b3
IvanBichan <ivanbichanfe@gmail.com>
2023-08-28 21:27:17 +0200

    fead:minor change

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { LoginForm } from '@/components/auth/login-form/login-form.tsx'
 import { SliderDemo } from '@/components/ui/slider/Slider'
 
 export function App() {
-  return <SliderDemo />
+  return (
+    <>
+      <SliderDemo />
+      <LoginForm />
+    </>
+  )
 }
