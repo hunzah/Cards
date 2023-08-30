8e1eadd4f23ada721a9a74f224d465f5b94b2150
iplatos <platonoff9168@gmail.com>
2023-08-30 14:10:36 +0300

    feat:add switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,52 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 import asd from '../src/assets/images/Layer 2.svg'
 
 import s from './components/ui/button/button.module.scss'
-import { Checkbox } from './components/ui/checkbox'
+import {Checkbox} from './components/ui/checkbox'
 
-import { Button } from '@/components/ui/button'
-import { Typography } from '@/components/ui/typography'
+import {Button} from '@/components/ui/button'
+import {Typography} from '@/components/ui/typography'
 import {Slider} from "@/components/ui/slider";
 import TabSwitcher from "@/components/ui/tab-switcher/tab-switcher";
 
 export function App() {
-  const [checked, setChecked] = useState(false)
-const switches =  [1,2,3]
-  return (
-    <div>
-      <Checkbox checked={checked} onChange={setChecked} disabled={false} />
-      <Typography variant="body2" component={'h3'}>
-        Subheading
-      </Typography>
-      <>asd</>
-      <Checkbox checked={checked} onChange={setChecked} text={'check-box'} disabled={false} />
-      Hello
-      <div>
-        <Button className={s.button}>
-          {' '}
-          <img src={asd} />
-          Hello
-        </Button>
-        <Button className={s.button} variant="tertiary">
-          <img src={asd} /> qweWeW
-        </Button>
-        <Button className={s.button} variant="tertiary">
-          qweWeW
-        </Button>
-          <Button className={s.button} variant="tertiary" disabled>
-          qweWeW
-        </Button>
+    const [checked, setChecked] = useState(false)
+    const switches = [{id: 1, switchTitle: "first", disabled: false}, {
+        id: 2,
+        switchTitle: "second",
+        disabled: false
+    }, {id: 3, switchTitle: "third", disabled: true}]
+    return (
+        <div>
+            <Checkbox checked={checked} onChange={setChecked} disabled={false}/>
+            <Typography variant="body2" component={'h3'}>
+                Subheading
+            </Typography>
+            <>asd</>
+            <Checkbox checked={checked} onChange={setChecked} text={'check-box'} disabled={false}/>
+            Hello
+            <div>
+                <Button className={s.button}>
+                    {' '}
+                    <img src={asd}/>
+                    Hello
+                </Button>
+                <Button className={s.button} variant="tertiary">
+                    <img src={asd}/> qweWeW
+                </Button>
+                <Button className={s.button} variant="tertiary">
+                    qweWeW
+                </Button>
+                <Button className={s.button} variant="tertiary" disabled>
+                    qweWeW
+                </Button>
 
-        <Button className={s.button} as={'a'} href={'/link'} variant="link">
-          as link
-        </Button>
-<TabSwitcher switches/>
-      </div>
-    </div>
-  )
+                <Button className={s.button} as={'a'} href={'/link'} variant="link">
+                    as link
+                </Button>
+                <TabSwitcher switches={switches}/>
+            </div>
+        </div>
+    )
 }
--- src/components/ui/tab-switcher/tab-switcher.tsx
+++ src/components/ui/tab-switcher/tab-switcher.tsx
@@ -4,20 +4,25 @@ import {useState} from "react";
 import {Typography} from "@/components/ui/typography";
 
 type PropsType = {
-    switches:number[]
+    switches:{id: number, switchTitle: string, disabled: boolean}[]
 }
 
-const TabSwitcher = () => {
-    const [active,setActive] = useState(true)
+const TabSwitcher = (props:PropsType) => {
+    const {switches} = props
+    const [active,setActive] = useState(1)
+
+    const activeSwitchChanger = (id:number) => {
+        setActive(id)
+        console.log(`switch number ${id} selected`)
+    }
+
     return (
         <div className={s.tabSwitchersContainer} >
             <Typography variant="body2" component={'h3'}>
                 Title
             </Typography>
             <div className={s.tabSwitchers}>
-                <button  className={s.tabSwitcher}>tabSwitcher</button>
-                <button className={s.tabSwitcher}>tabSwitcher</button>
-                <button onClick={()=>console.log("asdasd")} className={s.tabSwitcher} disabled>tabSwitcher</button>
+                {switches.map(sw=><button onClick={()=>activeSwitchChanger(sw.id)} key={sw.id} className={sw.id===active? s.activeTabSwitcher: s.tabSwitcher} disabled={sw.disabled}>{sw.switchTitle}</button>)}
             </div>
         </div>
     );
