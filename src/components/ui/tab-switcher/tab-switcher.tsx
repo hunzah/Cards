f459a7c75445ca0a658169d8824c01c6108c62a9
IvanBichan <ivanbichanfe@gmail.com>
2023-09-23 01:09:23 +0200

    feat: change style for tab-swither

--- src/components/ui/tab-switcher/tab-switcher.tsx
+++ src/components/ui/tab-switcher/tab-switcher.tsx
@@ -1,34 +1,39 @@
-import {useEffect, useState} from 'react'
+import { useState } from 'react'
+
+import { clsx } from 'clsx'
+import { useDispatch } from 'react-redux'
 
 import s from './tab-switcher.module.scss'
 
 import { Typography } from '@/components/ui/typography'
-import { useAppDispatch, useAppSelector } from '@/hooks'
-import { setMeUserId } from '@/services/auth/auth.slice'
-import {useDispatch} from "react-redux";
-import {updateCurrentPage} from "@/services/decks/decks.slice";
+import { updateCurrentPage } from '@/services/decks/decks.slice'
 
 type PropsType = {
   switches: { id: string; switchTitle: string }[]
   setSortId: (is: string) => void
-    sortId?:string
+  sortId?: string
+  title: string
+  className?: string
 }
-
+//todo sortId on 20 line I Commented it out so the error doesn't appear
 export const TabSwitcher = (props: PropsType) => {
-  const { switches, setSortId,sortId } = props
-const dispatch = useDispatch()
-  const [active, setActive] = useState("all"  )
+  const { switches, setSortId, /*sortId,*/ title, className } = props
+  const dispatch = useDispatch()
+  const [active, setActive] = useState('all')
 
   const activeSwitchChanger = (switchTitle: string, sortId: string) => {
-          setActive(switchTitle)
-          setSortId(sortId)
-      dispatch(updateCurrentPage(1))
+    setActive(switchTitle)
+    setSortId(sortId)
+    dispatch(updateCurrentPage(1))
+  }
+  const classNames = {
+    tabSwitchersContainer: clsx(s.tabSwitchersContainer, className),
   }
 
   return (
-    <div className={s.tabSwitchersContainer}>
+    <div className={classNames.tabSwitchersContainer}>
       <Typography variant="body2" component={'h3'}>
-        Title
+        {title}
       </Typography>
       <div className={s.tabSwitchers}>
         {switches.map(sw => (
