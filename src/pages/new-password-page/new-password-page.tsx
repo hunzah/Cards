9ad33b960b5ed65ff76bcdd79282edd4f6763915
IvanBichan <ivanbichanfe@gmail.com>
2023-09-22 14:37:09 +0200

    feat: delete comment

--- src/pages/new-password-page/new-password-page.tsx
+++ src/pages/new-password-page/new-password-page.tsx
@@ -9,7 +9,6 @@ export type PasswordData = {
 export const NewPasswordPage = () => {
   const { code } = useParams()
   const [resetPassword, { isSuccess, error }] = useResetPasswordMutation({})
-  //b2c47eb9-50fa-427c-b53c-15fef4bdc92c
 
   const createNewPasswordHandler = (data: PasswordData) => {
     resetPassword({ token: code, password: data.password })
