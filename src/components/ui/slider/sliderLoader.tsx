832a44b41a3710af266a0a2d377030e426906f33
iplatos <platonoff9168@gmail.com>
2023-09-23 15:00:29 +0300

    feat: slider loader span

---
+++ src/components/ui/slider/sliderLoader.tsx
@@ -0,0 +1,17 @@
+import s from "./sliderLoader.module.scss"
+import {useAppSelector} from "@/hooks";
+export const SliderLoader = () => {
+    const sliderValues = useAppSelector(state => state.slider)
+    return (
+        <div className={s.sliderLoader}>
+              <span className={s.SliderLoaderValuesNumber}>
+       {sliderValues.sliderCurrentValues[0] || "..."}
+      </span>
+            <div className={s.sliderText}><div className={s.leftThumb}></div>loading
+                <div className={s.rightThumb}></div></div>
+            <span className={s.SliderLoaderValuesNumber}>
+        {sliderValues.sliderCurrentValues[1] || "..."}
+      </span>
+        </div>
+    )
+}
